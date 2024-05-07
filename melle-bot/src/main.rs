c38abba459a1e5a40ba3aebf615241de0061ce6d
Niklas Grundstrom <niklas_g_92@hotmail.com>
2024-05-07 20:56:15 +0200

    fixed new function !vadskullepassamig

--- melle-bot/src/main.rs
+++ melle-bot/src/main.rs
@@ -5,6 +5,7 @@ use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 use shuttle_runtime::SecretStore;
 use tracing::{error, info};
+use rand::{thread_rng, Rng};
 
 struct Bot;
 
@@ -16,8 +17,8 @@ impl EventHandler for Bot {
                 error!("Error sending message: {:?}", e);
             }
         }
-        if msg.content == "!vadskajagspela" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
+        if msg.content == "!vadskullepassamig" {
+            if let Err(e) = msg.channel_id.say(&ctx.http, string_builder()).await {
                 error!("Error sending message: {:?}", e);
             }
         }
@@ -46,4 +47,47 @@ async fn serenity(
         .expect("Err creating client");
 
     Ok(client.into())
+}
+
+fn string_builder() -> String {
+    let build = get_random_build();
+    let champ = get_random_champ();
+    let role = get_random_role();
+
+    format!("Du borde testa {} {} {}, deeet hade passat DIG!", build, champ, role)
+}
+
+fn get_random_champ() -> String {
+    let champs = "Ahri,Akali,Alistar,Amumu,Anivia,Annie,Ashe,Azir,Akshan,Aurelion Sol,Aphelios,Blitzcrank,Brand,Braum,Bard,Belveth,Caitlyn,Cassiopeia,Cho'Gath,Corki,Camille,Darius,Diana,Dr. Mundo,Draven,Elise,Evelynn,Ekko,Ezreal,Fiddlesticks,Fiora,Fizz,Galio,Gangplank,Garen,Gnar,Gragas,Graves,Gwen,Hecarim,Heimerdinger,Irelia,Illaoi,Ivern,Janna,Jarvan IV,Jax,Jayce,Jinx,Jhin,Kalista,Karma,Karthus,Kassadin,Katarina,Kindred,Kayle,Kennen,Kha'Zix,Kog'Maw,Kled,Kayn,Kai'sa,K’Sante,LeBlanc,Lee Sin,Leona,Lissandra,Lucian,Lulu,Lux,Lillia,Malphite,Malzahar,Maokai,Master Yi,Milio,Miss Fortune,Mordekaiser,Morgana,Nami,Nasus,Nautilus,Nidalee,Nocturne,Nunu,Nilah,Neeko,Olaf,Orianna,Ornn,Pantheon,Poppy,Pyke,Quinn,Qiyana,Rammus,Rek'Sai,Renekton,Rengar,Riven,Rumble,Ryze,Renata,Rell,Rakan,Sejuani,Shaco,Shen,Shyvana,Singed,Sion,Sivir,Skarner,Sona,Soraka,Swain,Syndra,Senna,Sett,Samira,Seraphine,Sylas,Talon,Taric,Teemo,Thresh,Tristana,Trundle,Tryndamere,Twisted Fate,Twitch,Tahm kench,Taliyah,Udyr,Urgot,Varus,Vayne,Veigar,Vel'Koz,Vi,Viktor,Vladimir,Volibear,Vex,Viego,Warwick,Wukong,Xerath,Xin Zhao,Xayah,Yasuo,Yorick,Yuumi,Yone,Zac,Zed,Ziggs,Zilean,Zyra,Zeri,Zoe";
+    let champs_vec: Vec<&str> = champs.split(",").collect();
+    let mut rng = thread_rng();
+    let random_index = rng.gen_range(0..champs_vec.len());
+
+    let selected_champ = champs_vec[random_index];
+
+    selected_champ.to_string()
+}
+
+fn get_random_build() -> String {
+    let builds = "AD,AP,Tank,On-Hit,Ability Haste";
+    let builds_vec: Vec<&str> = builds.split(",").collect();
+
+    let mut rng = thread_rng();
+    let random_index = rng.gen_range(0..builds_vec.len());
+
+    let selected_build = builds_vec[random_index];
+
+    selected_build.to_string()
+}
+
+fn get_random_role() -> String {
+    let role = "Top,Mid,Jungle,Bot,Supp";
+    let role_vec: Vec<&str> = role.split(",").collect();
+
+    let mut rng = thread_rng();
+    let random_index = rng.gen_range(0..role_vec.len());
+
+    let selected_role = role_vec[random_index];
+
+    selected_role.to_string()
 }
